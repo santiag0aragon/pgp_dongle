5ba2728eff39b3f69f5cc3332f8896a9e749b3b5
Santiago Aragón <santiago.aragon@datata.mx>
2016-03-02 20:19:57 +0100

    key export

--- pgp_keymgnt.py
+++ pgp_keymgnt.py
@@ -1,6 +1,6 @@
 import os
 import gnupg
-
+# KEY GEN
 os.system('rm -rf /home/testgpguser/gpghome')
 gpg = gnupg.GPG(gnupghome='/home/testgpguser/gpghome')
 input_data = gpg.gen_key_input(
@@ -8,3 +8,11 @@ input_data = gpg.gen_key_input(
     passphrase='my passphrase')
 key = gpg.gen_key(input_data)
 print key
+
+# KEY EXPORT
+gpg = gnupg.GPG(gnupghome='/home/testgpguser/gpghome')
+ascii_armored_public_keys = gpg.export_keys(key)
+ascii_armored_private_keys = gpg.export_keys(key, True)
+with open('mykeyfile.asc', 'w') as f:
+    f.write(ascii_armored_public_keys)
+    f.write(ascii_armored_private_keys)
