686d2056c6aa3d023b32101d4fbf4c1a59954af4
Santiago Aragón <santiago.aragon@datata.mx>
2016-03-02 23:14:04 +0100

    pgp test

--- pgp_keymgnt.py
+++ pgp_keymgnt.py
@@ -1,33 +1,35 @@
 import os
 import gnupg
 from pprint import pprint
+
+path = './testgpguser/gpghome'
 # KEY GEN
 print '###########################################'
-os.system('rm -rf /home/testgpguser/gpghome')
-gpg = gnupg.GPG(gnupghome='/home/testgpguser/gpghome')
+os.system('rm -rf %s'% s)
+gpg = gnupg.GPG(gnupghome=path)
 input_data = gpg.gen_key_input(
     name_email='testgpguser@mydomain.com',
     passphrase='my passphrase')
 key = gpg.gen_key(input_data)
 print key
 
-# KEY EXPORT
-print '###########################################'
-gpg = gnupg.GPG(gnupghome='/home/testgpguser/gpghome')
-ascii_armored_public_keys = gpg.export_keys(key)
-ascii_armored_private_keys = gpg.export_keys(key, True)
-with open('mykeyfile.asc', 'w') as f:
-    f.write(ascii_armored_public_keys)
-    f.write(ascii_armored_private_keys)
-# KEY IMPORT
-print '###########################################'
-gpg = gnupg.GPG(gnupghome='/home/testgpguser/gpghome')
-key_data = open('mykeyfile.asc').read()
-import_result = gpg.import_keys(key_data)
-pprint(import_result.results)
+# # KEY EXPORT
+# print '###########################################'
+# gpg = gnupg.GPG(gnupghome=path)
+# ascii_armored_public_keys = gpg.export_keys(key)
+# ascii_armored_private_keys = gpg.export_keys(key, True)
+# with open('mykeyfile.asc', 'w') as f:
+#     f.write(ascii_armored_public_keys)
+#     f.write(ascii_armored_private_keys)
+# # KEY IMPORT
+# print '###########################################'
+# gpg = gnupg.GPG(gnupghome=path)
+# key_data = open('mykeyfile.asc').read()
+# import_result = gpg.import_keys(key_data)
+# pprint(import_result.results)
 # STRING ENC
 print '###########################################'
-gpg = gnupg.GPG(gnupghome='/home/testgpguser/gpghome')
+gpg = gnupg.GPG(gnupghome=path)
 unencrypted_string = 'Who are you? How did you get in my house?'
 encrypted_data = gpg.encrypt(unencrypted_string, 'testgpguser@mydomain.com')
 encrypted_string = str(encrypted_data)
@@ -38,7 +40,7 @@ print 'unencrypted_string: ', unencrypted_string
 print 'encrypted_string: ', encrypted_string
 # STRING DEC
 print '###########################################'
-gpg = gnupg.GPG(gnupghome='/home/testgpguser/gpghome')
+gpg = gnupg.GPG(gnupghome=path)
 unencrypted_string = 'Who are you? How did you get in my house?'
 encrypted_data = gpg.encrypt(unencrypted_string, 'testgpguser@mydomain.com')
 encrypted_string = str(encrypted_data)
@@ -50,7 +52,7 @@ print 'stderr: ', decrypted_data.stderr
 print 'decrypted string: ', decrypted_data.data
 # FILE ENC
 print '###########################################'
-gpg = gnupg.GPG(gnupghome='/home/testgpguser/gpghome')
+gpg = gnupg.GPG(gnupghome=path)
 open('my-unencrypted.txt', 'w').write('You need to Google Venn diagram.')
 with open('my-unencrypted.txt', 'rb') as f:
     status = gpg.encrypt_file(
@@ -62,7 +64,7 @@ print 'status: ', status.status
 print 'stderr: ', status.stderr
 # FILE DEC
 print '###########################################'
-gpg = gnupg.GPG(gnupghome='/home/testgpguser/gpghome')
+gpg = gnupg.GPG(gnupghome=path)
 with open('my-encrypted.txt.gpg', 'rb') as f:
     status = gpg.decrypt_file(f, passphrase='my passphrase', output='my-decrypted.txt')
 
