f9089d87262bb01ec0057c2bd495375819dd1b88
Santiago Aragón <santiago.aragon@datata.mx>
2016-04-05 23:52:53 +0200

    sgn vrify modes

--- socket_utils.py
+++ socket_utils.py
@@ -1,13 +1,16 @@
 import socket
 import struct
 
+
 def recv_one_message(sock):
     lengthbuf = recvall(sock, 4)
     length, = struct.unpack('!I', lengthbuf)
     lengthbuf = recvall(sock, 4)
     mode, = struct.unpack('!I', lengthbuf)
-    data = {'mode': mode, 'data': recvall(sock, length)}
+    data = {'mode': mode,
+            'data': recvall(sock, length)}
     return data
+
 
 def recvall(sock, count):
     buf = b''
@@ -19,14 +22,25 @@ def recvall(sock, count):
     return buf
 
 
+def send_one_sgn(sock, data):
+    send_one_message(sock, data, 4)
+
+
+def send_one_vrf(sock, data):
+    send_one_message(sock, data, 3)
+
+
 def send_one_resp(sock, data):
     send_one_message(sock, data, 2)
+
 
 def send_one_dec(sock, data):
     send_one_message(sock, data, 1)
 
+
 def send_one_enc(sock, data):
     send_one_message(sock, data, 0)
+
 
 def send_one_message(sock, data, mode):
     length = len(data)
